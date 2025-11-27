f22ca75ace98d24d33c2d251fda97a294b67c181
Stoian <78989110+Stoian01@users.noreply.github.com>
2025-11-27 19:02:29 -0300

    Inter cstoian (#46)
    * Delete css/styleSobre.css
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update style.css
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update style2.css
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete img directory
    
    Está nos arquivos midia
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete js/script.js
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete js/script2.js
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete js/data/translation/en.json
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete js/data/translation/pt.json
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Delete midia directory
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update about.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update april.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update blue.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update green.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update index.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update june.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update may.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update orange.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update other.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update red.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update yellow.html
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Update README.md
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Stoian <78989110+Stoian01@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # interdisciplinar
 Desenvolvimento Web Interdisciplinar, Carlos, Igor, Luiz Fernando, Maria Luiza
-test create branch
+
+27/11/2025
+Structure Page
+
+index.html > 
+           > april.html > blue.html > green.html
+           > may.html > yellow.html
+           > june.html > red.html > orange.html
+css: > style.css 
+     > style2.css
+js:  > script.js
+> data:  en.json 
+         pt.json
+midia: 
+> audio: >
+    about >
+> img:
+    > abril:
+        azul:
+        verde:
+    > index:
+    > junho:
+        >laranja: 
+        >vermelho:
+    > logo:
+    > maio:
+        >yellow:
+    > outro:
+    > sobre:
+        >icons
+> video:
+    > index:
+27/11/2025 Files review
+> JS script, translations
+> Midia: Imgs att .webp, index, may, blue, green, april, yellow, june, red, orange, logo, other, review others alter again.
+> Allpages: review structure and id's correlation in css and js.
--- js/script.js
+++ js/script.js
@@ -1,35 +1,14 @@
-openMenu.addEventListener('click', () => {
-  menu.style.display = 'flex'
-
-  menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-  openMenu.style.display = 'none'
-
-  setTimeout(() => {
-    menu.style.opacity = '1'
-    menu.style.right = '0'
-  }, 10)
-
-})
-
-closeMenu.addEventListener('click', () => {
-  menu.style.opacity = '0'
-
-  menu.style.left = (menu.offsetWidth * -1) + 'px'
-
-  setTimeout(() => {
-    menu.removeAttribute('style')
-    openMenu.removeAttribute('style')
-  }, 200)
-
-})
-
 const LANGUAGE_KEY = 'siteLanguage';
 const player = document.getElementById("player");
+const imgMaio = document.getElementById("img-maio");
+const imgAbril = document.getElementById("img-abril");
+const abrilVerde = document.getElementById("abril-verde-img");
+const abrilAzul = document.getElementById("abril-azul-img");
+const imgJunho = document.getElementById("img-junho");
+const indexAbril = document.getElementById("index-img-abril");
+const indexMaio = document.getElementById("index-img-maio");
+const indexJunho = document.getElementById("index-img-junho");
 
-/**
-  @param {string} lang 
- */
 async function setLanguage(lang) {
   localStorage.setItem(LANGUAGE_KEY, lang);
   const filePath = `js/data/translation/${lang}.json`;
@@ -53,6 +32,38 @@ async function setLanguage(lang) {
     if (player && translations['audio-src']) {
       player.src = translations['audio-src'];
       player.load();
+    }
+    if (imgMaio && translations['img-maio']) {
+      imgMaio.src = translations['img-maio'];
+      imgMaio.load()
+    }
+    if (imgAbril && translations['img-abril']) {
+      imgAbril.src = translations['img-abril'];
+      imgAbril.load()
+    }
+    if (abrilVerde && translations['abril-verde-img']) {
+      abrilVerde.src = translations['abril-verde-img'];
+      abrilVerde.load()
+    }
+    if (abrilAzul && translations['abril-azul-img']) {
+      abrilAzul.src = translations['abril-azul-img'];
+      abrilAzul.load()
+    }
+    if (imgJunho && translations['img-junho']) {
+      imgJunho.src = translations['img-junho'];
+      imgJunho.load()
+    }
+
+    if (indexAbril && translations['index-img-abril']) {
+      indexAbril.src = translations['index-img-abril'];
+    }
+
+    if (indexMaio && translations['index-img-maio']) {
+      indexMaio.src = translations['index-img-maio'];
+    }
+
+    if (indexJunho && translations['index-img-junho']) {
+      indexJunho.src = translations['index-img-junho'];
     }
 
   } catch (error) {
